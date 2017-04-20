d8281a0caefc0e5ae4c119759579aa93ed006333
Rogh Ian <roghiansawangen111@gmail.com>
2017-04-20 13:01:58 +0800

    modified view

--- resources/views/adminpetra/AdminDashboard.blade.php
+++ resources/views/adminpetra/AdminDashboard.blade.php
@@ -153,7 +153,7 @@ desired effect
         <ul class="sidebar-menu">
           <li class="header">HEADER</li>
           <!-- Optionally, you can add icons to the links -->
-          <li class="active"><a href="#"><i class="fa fa-list-ul"></i><span>Projects</span></a></li>
+          <li class="active"><a href="/project"><i class="fa fa-list-ul"></i><span>Projects</span></a></li>
           <li><a href="#"><i class="fa fa-tasks"></i> <span>Task Details</span></a></li>
           <li class="treeview">
             <a href="#"><i class="fa fa-users"></i> <span>Members</span>
@@ -162,7 +162,7 @@ desired effect
               </span>
             </a>
             <ul class="treeview-menu">
-              <li><a href="#">Member List</a></li>
+              <li><a href="/user">Member List</a></li>
               <li><a href="#">Member Details</a></li>
             </ul>
           </li>
